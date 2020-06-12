b7f9bb3b8912f0c7442f6eddcb5fd525201b96f9
Andre <ajparker000@gmail.com>
2020-06-11 17:23:26 -0700

    updated data section

---
+++ script.js
@@ -0,0 +1,36 @@
+const data = [
+  {
+    id: '0',
+    productName: 'Auto Insurance',
+    description:
+      'Auto insurance is an agreement between you and your insurance provider about what would happen in the event of an accident. It protects you from financial losses in the event of incidents covered by your policy. Rather than being on the hook for things like repair, replacement costs and even medical bills, you pay a car insurance premium to your insurance provider and they will help cover the costs based upon the coverage and limits you choose.',
+  },
+  {
+    id: '1',
+    productName: 'Home Insurance',
+    description:
+      "Homeowners insurance is a form of property insurance that covers losses and damages to an individual's house and to assets in the home. Homeowners insurance also provides liability coverage against accidents in the home or on the property. ",
+  },
+  {
+    id: '2',
+    productName: 'Renters Insurance',
+    description:
+      "Renters' insurance, often called tenants' insurance, is an insurance policy that provides some of the benefits of homeowners' insurance, but does not include coverage for the dwelling, or structure, with the exception of small alterations that a tenant makes to the structure.",
+  },
+  {
+    id: '3',
+    productName: 'Life Insurance',
+    description:
+      'Life insurance (or life assurance, especially in the Commonwealth of Nations) is a contract between an insurance policy holder and an insurer or assurer, where the insurer promises to pay a designated beneficiary a sum of money (the benefit) in exchange for a premium, upon the death of an insured person (often the policy holder).',
+  },
+  {
+    id: '4',
+    productName: 'Health Insurance',
+    description:
+      'Health Insurance is a loss by illness or bodily injury. Health insurance provides coverage for medicine, visits to the doctor or emergency room, hospital stays and other medical expenses. Policies differ in what they cover, the size of the deductible and/or co-payment, limits of coverage and the options for treatment available to the policyholder.',
+  },
+];
+
+data.forEach((el) => {
+  console.log(el);
+});

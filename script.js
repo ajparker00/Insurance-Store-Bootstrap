8620f8ade182a1557f09c66e88e872fd555a8a53
Andre <ajparker000@gmail.com>
2020-06-24 17:24:31 -0700

    adding Javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('working');
+const button = document.querySelector('.target');
+
+button.addEventListener('click', (event) => {
+  alert('Button Clicked');
+});
+
+// console.log('file complete');
+// $('#target').click(function () {
+//   console.log('what happen');
+//   alert('Button clicked');
+// });

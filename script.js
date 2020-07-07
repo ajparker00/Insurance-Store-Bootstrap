cdbb244e8dadeae5118430fd2d9745289e4c00ad
Andre <ajparker000@gmail.com>
2020-07-06 19:00:50 -0700

    updated form validation

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
 const submit = document.getElementById('submit');
-const firstName = document.getElementById('firstName');
-const lastName = document.getElementById('lastName');
 const email = document.getElementById('email');
-const form = document.getElementById('form');
-const errorElement = document.getElementById('error');
 
 form.addEventListener('submit', (event) => {
-  let messages = [];
-  if (firstName.value === '' || firstName.value == null) {
-    messages.push('First Name is required');
-  }
-  if (lastName.value === '' || lastName.value == null) {
-    messages.push('Last Name is required');
-  }
   if (email.value === '') {
     alert('Please enter email!');
+  } else {
+    submit.addEventListener('click', (event) => {
+      alert(
+        'Your form has been submitted. Someone will reach out to you within 24hrs to 48hrs!'
+      );
+    });
   }
-
-  if (messages.length > 0) {
-    event.preventDefault();
-    errorElement.innerText = messages.join(', ');
-  }
+  event.preventDefault();
 });
-
-// submit.addEventListener('click', (event) => {
-//   alert(
-//     'Your form has been submitted. Someone will reach out to you within 24hrs to 48hrs!'
-//   );
-// });

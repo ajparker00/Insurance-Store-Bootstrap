6e05563b977f19140570f1fbeca922b21e885f86
Andre <ajparker000@gmail.com>
2020-07-16 17:01:26 -0700

    Updated submit click event

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', (event) => {
   } else {
     submit.addEventListener('click', (event) => {
       alert(
-        'Your form has been submitted. Someone will reach out to you within 24hrs to 48hrs!'
+        'Your form has been submitted. Someone will reach out to you within 24hrs or the next business day!'
       );
     });
   }
